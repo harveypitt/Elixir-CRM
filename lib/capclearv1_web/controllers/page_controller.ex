9895bff15524763284957ea65e7d58a11f99279b
harveypitt <harvp@proton.me>
2024-12-29 17:43:22 +0000

    removed bloat

--- lib/capclearv1_web/controllers/page_controller.ex
+++ lib/capclearv1_web/controllers/page_controller.ex
@@ -6,12 +6,4 @@ defmodule Capclearv1Web.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
-
-  def users(conn, _params) do
-    users = [
-      %{id: 1, name: "Harvey", email: "harvey@email.com"},
-      %{id: 2, name: "Francis", email: "francis@email.com"}
-    ]
-    json(conn, %{users: users})
-  end
 end
